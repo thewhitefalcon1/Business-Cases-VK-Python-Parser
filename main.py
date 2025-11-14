1094a99b051e1c3ae9f89d4ebb57554e6c8f54d0
user111 <matenkaalex@gmail.com>
2025-11-14 15:05:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+url = 'https://ads.vk.com/cases'
+
+response = requests.get(url)
+html = response.text
+
+soup = BeautifulSoup(html, 'html.parser')
+cards = soup.find_all(class_="case-card_wrapper__F9fy_")
+
+cases_massive = []
+
+for card in cards:
+    a = dict()
+    relative_link = card.get("href")   
+    title = card.get_text(strip=True)   
+    true_link = urljoin(url, relative_link)
+    a = {'Название кейса': title[10:] , 'Дата публикации': title[:9] , 'Ссылка': true_link}
+    cases_massive.append(a)
+
+for i, case in enumerate(cases_massive, start=1):
+    print(f"Кейс №{i}")
+    print(f"Название: {case['Название кейса']}")
+    print(f"Дата: {case['Дата публикации']}")
+    print(f"Ссылка: {case['Ссылка']}")
+    print("_" * 40)
